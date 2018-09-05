8b05c2d9e964da28f3aa7e140538728e85f72bc9
Drew Davis <kalaborative94@gmail.com>
2018-09-05 14:50:59 -0500

    Heroku conventions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from api import get_recent_top_players, load_top_players
+from os import environ
 
 app = Flask(__name__)
 
@@ -13,4 +14,7 @@ def index():
 	return render_template("index.html")
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	# convention to run on Heroku
+	port = int(environ.get("PORT", 5000))
+	# run the app available anywhere on the network, on debug mode
+	app.run(host="0.0.0.0", port=port, debug=True)
